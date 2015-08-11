278c3f39d4201e930f0a4a78456094c70ec86ec1
Steve Hannah <steve@weblite.ca>
2015-08-10 22:50:21 -0700

    Changed client server URL to public site

--- social-network/src/com/codename1/demos/socialnet/SocialClient.java
+++ social-network/src/com/codename1/demos/socialnet/SocialClient.java
@@ -29,7 +29,7 @@ import java.util.Map;
  */
 public class SocialClient {
     private String token;
-    private static String URL = "http://localhost/social_network";
+    private static String URL = "http://cn-social-network-demo.weblite.ca";
     private String username;
     
     
